caa106e5bd6225b4e47fb27da178e83676fa0d1b
Ananochi <rloc@itu.dk>
2014-03-18 14:41:48 +0100

    More DB functionality

---
+++ ISeeN/ISeeNDB/DBfunc/UserDB.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISeeN_DB
+{
+    class UserDB
+    {
+        private static ISeeNDbContext _context;
+
+        public static void AddUser(ISeeN_DB.User user)
+        {
+            _context = new ISeeNDbContext();
+            using (var db = _context)
+            {
+                var _user = new ISeeN_DB.User()
+                {
+                    Name = user.Name,
+                    Bio = user.Bio,
+                    City = user.City,
+                    Country = user.Country,
+                    Email = user.Email,
+                    Id = user.Id,
+                    IsAdmin = user.IsAdmin,
+                    Password = user.Password
+                };
+
+                db.Users.Add(_user);
+                db.SaveChanges();
+
+            }
+        }
+
+        public IList<int> GetAllUsers()
+        {
+            // implement
+            return null;
+        }
+
+        public int GetUserById(ISeeN_DB.User user)
+        {
+            // implement
+            return 0;
+        }
+    }
+}
--- ISeeN/ISeeNDB/MainProgram.cs
+++ ISeeN/ISeeNDB/MainProgram.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
@@ -12,32 +14,30 @@ namespace ISeeN_DB
     {
         static void Main(string[] args)
         {
+            // testing UserDB.AddUser()
+            var user = new ISeeN_DB.User()
+            {
+                Name = "Rasmus Løbner Christensen",
+                Bio = "Flot fyr i sin bedste alder",
+                City = "Frederiksberg",
+                Country = "Denmark",
+                Email = "rloc@itu.dk",
+                Id = 1337,
+                IsAdmin = true,
+                Password = "Hemmeligt!"
+            };
+
+            // calling the method
+            UserDB.AddUser(user);
+
             using (var db = new ISeeNDbContext())
             {
-                var user = new ISeeN_DB.User { Name = "Carsten" };
-                var potato = new ISeeN_DB.Potato { Id = 10, EncPassword = "Hemmeligt Kodeord!" };
-                var reminder = new ISeeN_DB.Reminder { Id = 10, Title = "Reminder!" };
-                var media = new ISeeN_DB.Media { Id = 10, Title = "MediaTitle" };
-                var statistic = new ISeeN_DB.Statistic { MediaId = 10 };
-                var movies = new ISeeN_DB.Movie { Director = "Steven S." };
-                var pictures = new ISeeN_DB.Picture { Author = "Chigal" };
-                var music = new ISeeN_DB.Music { Artist = "Slim Shady" };
-                db.Users.Add(user);
-                db.Potatoes.Add(potato);
-                db.Reminders.Add(reminder);
-                db.Medias.Add(media);
-                db.Statistics.Add(statistic);
-                db.Movies.Add(movies);
-                db.Pictures.Add(pictures);
-                db.Music.Add(music);
-                db.SaveChanges();
 
-                // Query to print out data
                 var query = from b in db.Users
-                            orderby b.Id
-                            select b;
+                    orderby b.Id
+                    select b;
 
-                Console.WriteLine("Current query output:");
+                // write result to nice console
                 foreach (var item in query)
                 {
                     Console.WriteLine(item.Name);
@@ -45,7 +45,7 @@ namespace ISeeN_DB
 
                 Console.WriteLine("Press any key to exit...");
                 Console.ReadKey();
-            } 
+            }
         }
     }
 }
