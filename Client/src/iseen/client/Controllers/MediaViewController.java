3bd166cbd23e8787728538e6978d6b23dbb93eeb
Ahmed Al Aqtash <ahmed.aqtash@gmail.com>
2014-05-19 17:27:42 +0200

    Fiksing mediaview

--- Client/src/iseen/client/Controllers/MediaViewController.java
+++ Client/src/iseen/client/Controllers/MediaViewController.java
@@ -1,16 +1,24 @@
 package iseen.client.Controllers;
 
+import iseen.client.Entities.MediaFormats.Media;
+import iseen.client.Entities.MediaFormats.MediaTypes;
+import iseen.client.Entities.MediaFormats.*;
 import iseen.client.Main;
 import iseen.client.Storage.Memory;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ResourceBundle;
 
 /**
@@ -20,11 +28,30 @@ public class MediaViewController implements Initializable {
     public Label BigTitle;
     public ImageView Image;
     public VBox Properties;
+    public TextArea MediaInfo;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         BigTitle.setText(Memory.CurrentMedia.Title);
         Image.setImage(new javafx.scene.image.Image(Memory.CurrentMedia.Image));
+
+        if (Memory.CurrentMedia.Type == MediaTypes.Types.MOVIE) {
+            Memory.CurrentMedia = new Movie();
+            MediaInfo.appendText(((Movie) Memory.CurrentMedia).Director + "\n");
+        }
+
+        if (Memory.CurrentMedia.Type == MediaTypes.Types.MUSIC) {
+            Memory.CurrentMedia = new Music();
+            MediaInfo.appendText(((Music) Memory.CurrentMedia).Artist + "\n");
+        }
+
+        if (Memory.CurrentMedia.Type == MediaTypes.Types.PICTURE) {
+            Memory.CurrentMedia = new Picture();
+            MediaInfo.appendText(((Picture) Memory.CurrentMedia).Painter + "\n");
+        }
+
+        MediaInfo.appendText(Memory.CurrentMedia.ReleaseDate.toString() + "\n");
+        MediaInfo.appendText(Memory.CurrentMedia.Description + "\n");
     }
 
     public void BreadCrumbHome(MouseEvent actionEvent) {
