d11a9a2a00318ee40f7896328171e512dc7e1674
Ananochi <rloc@itu.dk>
2014-05-19 11:30:30 +0200

    Added test structure

---
+++ ISeeN/ISeeNIIS_Test/ISeendatabaseTest.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ISeeNIIS_Test
+{
+    [TestClass]
+    public class ISeendatabaseTest
+    {
+
+        #region mediaDB Functions
+        [TestMethod]
+        public void AddMediaTest()
+        {
+        }
+
+        [TestMethod]
+        public void SearchTextTest()
+        {            
+        }
+
+        [TestMethod]
+        public void SearchTypeTest()
+        {   
+        }
+
+        [TestMethod]
+        public void SearchBothTest()
+        {
+        }
+
+        [TestMethod]
+        public void GetAllTest()
+        {
+        }
+
+        [TestMethod]
+        public void GetMediaForIdTest()
+        {
+        }
+
+        [TestMethod]
+        public void DeleteMediaTest()
+        {
+        }
+
+        [TestMethod]
+        public void EditMediaTest()
+        {
+        }
+
+        [TestMethod]
+        public void RentMediaTest()
+        {
+        }
+
+        [TestMethod]
+        public void GetStatsForIdTest()
+        {
+        }
+
+        #endregion
+
+        #region RentalsDB Functions
+
+        [TestMethod]
+        public void RentalsForUserTest()
+        {
+        }
+
+        [TestMethod]
+        public void CheckUserRentedTest()
+        {
+        }
+
+        #endregion
+
+        #region UserDB Functions
+
+        [TestMethod]
+        public void AddUserTest()
+        {
+        }
+
+        [TestMethod]
+        public void LoginUserTest()
+        {
+        }
+
+        [TestMethod]
+        public void EditAccountTest()
+        {
+        }
+
+        [TestMethod]
+        public void GetUserByPotatoTest()
+        {
+        }
+
+        [TestMethod]
+        public void DeleteUserTest()
+        {
+        }
+
+        [TestMethod]
+        public void PotatoHandleTest()
+        {
+        }
+        #endregion
+    }
+}
---
+++ ISeeN/ISeeNIIS_Test/ISeenserviceTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ISeeNIIS_Test
+{
+    [TestClass]
+    public class ISeenserviceTest
+    {
+        #region Account Operations Test
+        [TestMethod]
+        public void SearchMediaTest()
+        {
+        }
+
+        [TestMethod]
+        public void CreateAccountTest()
+        {
+        }
+
+        [TestMethod]
+        public void EditAccountTest()
+        {
+        }
+
+        [TestMethod]
+        public void GetAccountTest()
+        {
+        }
+
+        [TestMethod]
+        public void AccountLoginTest()
+        {
+        }
+
+        [TestMethod]
+        public void DeleteAccountTest()
+        {
+        }
+
+        #endregion
+
+        #region Media Operations Test
+
+        [TestMethod]
+        public void GetAllMediaTest()
+        {
+        }
+
+        [TestMethod]
+        public void GetMediaForIdTest()
+        {
+        }
+
+        [TestMethod]
+        public void GetStatsForIdTest()
+        {
+        }
+
+        [TestMethod]
+        public void RentMediaByIdTest()
+        {
+        }
+
+        [TestMethod]
+        public void CreateNewMediaTest()
+        {
+        }
+
+        [TestMethod]
+        public void EditMediaTest()
+        {
+        }
+
+        [TestMethod]
+        public void DeleteMediaTest()
+        {
+        }
+
+        #endregion
+
+        #region Rentals Operations Test
+
+        [TestMethod]
+        public void GetFileForRentalTest()
+        {
+        }
+
+        [TestMethod]
+        public void GetRentalsForUserTest()
+        {
+        }
+
+        [TestMethod]
+        public void CheckUserRented()
+        {
+        }
+
+        #endregion
+
+    }
+}
